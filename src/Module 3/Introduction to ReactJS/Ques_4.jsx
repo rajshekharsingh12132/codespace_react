2b19ce53af09f10e5a6f40606a8decf87ce8f983
rajshekharsingh12132 <127713232+rajshekharsingh12132@users.noreply.github.com>
2025-05-26 04:16:29 +0000

    modified:   src/Module 3/Introduction to ReactJS/Ques_4.jsx

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -1,17 +1,18 @@
-// JSX Rules
-// Description: Identify and correct errors in incorrect JSX syntax.
+import React from 'react';
 
-function Error() {
-    return (
-        <div>
-            <h1>Unclosed tag
-            <p>Self-closing tag example <img src="example.png"></img></p>
-            <ul>
-                <li>List item 1
-                <li>List item 2
-            </ul>
-        </div>
-    );
-}
+const FixedComponent = () => {
+  return (
+    <div>
+      <h1>Unclosed tag fixed</h1>
+      <p>
+        Self-closing tag example <img src="example.png" alt="Example" />
+      </p>
+      <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+      </ul>
+    </div>
+  );
+};
 
-
+export default FixedComponent;
