f40dd319c1496eadf39f927aa37ad94fc52a814e
rajshekharsingh12132 <127713232+rajshekharsingh12132@users.noreply.github.com>
2025-05-27 03:48:29 +0000

    modified:   backend/Module 5/Introduction to Node.js & Express.js/5_error_handling_middleware.js

--- backend/Module 5/Introduction to Node.js & Express.js/5_error_handling_middleware.js
+++ backend/Module 5/Introduction to Node.js & Express.js/5_error_handling_middleware.js
@@ -1,5 +1,3 @@
-// Error Handling Middleware in Express.js
-// Description: Implement custom error handling middleware in an Express application.
 const express = require('express');
 const app = express();
 const PORT = 3000;
