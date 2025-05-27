893a14b93b295cff4c4997d5efb08f666edfe313
rajshekharsingh12132 <127713232+rajshekharsingh12132@users.noreply.github.com>
2025-05-27 03:26:12 +0000

    modified:   backend/Module 5/Introduction to Node.js & Express.js/1_hello.js

--- backend/Module 5/Introduction to Node.js & Express.js/1_hello.js
+++ backend/Module 5/Introduction to Node.js & Express.js/1_hello.js
@@ -1,8 +1,4 @@
-// Running JavaScript in Node.js
-// Description: Demonstrate how to run a simple JavaScript file using Node.js.
+// hello.js
+console.log("Hello, Node.js");
 
-// Steps:
-// 1. Write a message "Hello, Node.js" in the console.
-// 2. Open a terminal and navigate to the directory containing hello.js .
-// 3. Run the JavaScript file using Node.js ......
 
